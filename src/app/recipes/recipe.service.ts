d51444377b4ae951b53e537172cc6c4d66972154
Aljamil1113 <aarazasaljamil@gmail.com>
2020-06-25 00:11:51 -0700

    Reactive Approach Angular Form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,12 +2,13 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-
+  recipesChanged = new Subject<Recipe[]>();
   recipe: Recipe[] = [
     new Recipe('A test recipe', 'Sample recipe',
     'https://assets.bonappetit.com/photos/5db1ce83358b460009148cb7/3:2/w_5120,c_limit/Basically-Spinach-Tarte-Recipe.jpg',
@@ -20,13 +21,27 @@ export class RecipeService {
     return this.recipe.slice();
   }
 
-  addIngredientsToShoppingList(ingredients: Ingredient[]){
-    this.shoppingList.addIngredients(ingredients);
-  }
-
   getRecipe(id: number) {
     return this.recipe[id];
   }
 
+  addRecipe(recipe: Recipe){
+    this.recipe.push(recipe);
+    this.recipesChanged.next(this.recipe.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipe[index] = newRecipe;
+    this.recipesChanged.next(this.recipe.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipe.splice(index, 1);
+    this.recipesChanged.next(this.recipe.slice());
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingList.addIngredients(ingredients);
+  }
 
 }
