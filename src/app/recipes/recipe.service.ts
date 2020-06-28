4d5212a7b1080047bf5c2178fa3d6f551f4c1764
Aljamil1113 <aarazasaljamil@gmail.com>
2020-06-27 18:06:09 -0700

    Http Request

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,13 +9,19 @@ import { Subject } from 'rxjs';
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  recipe: Recipe[] = [
-    new Recipe('A test recipe', 'Sample recipe',
-    'https://assets.bonappetit.com/photos/5db1ce83358b460009148cb7/3:2/w_5120,c_limit/Basically-Spinach-Tarte-Recipe.jpg',
-    [new Ingredient('Noodles', 4), new Ingredient('Cheese', 3)])
-  ];
+  // recipe: Recipe[] = [
+  //   new Recipe('A test recipe', 'Sample recipe',
+  //   'https://assets.bonappetit.com/photos/5db1ce83358b460009148cb7/3:2/w_5120,c_limit/Basically-Spinach-Tarte-Recipe.jpg',
+  //   [new Ingredient('Noodles', 4), new Ingredient('Cheese', 3)])
+  // ];
+  private recipe: Recipe[] = []
 
   constructor(private shoppingList: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]){
+    this.recipe = recipes;
+    this.recipesChanged.next(this.recipe.slice());
+  }
 
   getRecipes(){
     return this.recipe.slice();
